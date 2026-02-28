e8b2c52dbabfa1c71455bff4993841f445568566
rrworkscom <rrworks010@gmail.com>
2026-03-01 00:15:41 +0700

    Modified

--- Practicum3/Course20.java
+++ Practicum3/Course20.java
@@ -5,11 +5,24 @@ public class Course20 {
     public String name;
     public int credit;
     public int hours;
+    
 
-    public Course20(String code, String name, int credit, int hours) {
-        this.code = code;
-        this.name = name;
-        this.credit = credit;
-        this.hours = hours;
+    public Course20() {
+    }
+
+    void addData (String code, String nm, int crd, int hrs) {
+        this.code  = code;
+        name = nm;
+        credit = crd;
+        hours = hrs;
+    }
+
+    void printInfo() {
+        System.out.println("Code     : " + code);
+        System.out.println("Name     : " + name);
+        System.out.println("Credit   : " + credit);
+        System.out.println("Hours    : " + hours);
     }
 }
+
+
