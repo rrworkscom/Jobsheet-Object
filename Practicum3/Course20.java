581e682b8265e08c467b2d2990b075c8de3bf4c0
rrworkscom <rrworks010@gmail.com>
2026-02-28 20:21:43 +0700

    Course Class

---
+++ Practicum3/Course20.java
@@ -0,0 +1,15 @@
+package Practicum3;
+
+public class Course20 {
+    public String code;
+    public String name;
+    public int credit;
+    public int hours;
+
+    public Course20(String code, String name, int credit, int hours) {
+        this.code = code;
+        this.name = name;
+        this.credit = credit;
+        this.hours = hours;
+    }
+}
