7fba96582dbcf240586d0e89dc68b89926043c4a
rrworkscom <rrworks010@gmail.com>
2026-02-28 20:01:31 +0700

    Updated

--- Practicum3/Student20.java
+++ Practicum3/Student20.java
@@ -5,4 +5,11 @@ public class Student20 {
     public String name;
     public String grade;
     public float gpa;
+
+    void printInfo() {
+        System.out.println("NIM     : " + nim);
+        System.out.println("Name    : " + name);
+        System.out.println("Grade   : " + grade);
+        System.out.println("GPA     : " + gpa);
+    }
 }
